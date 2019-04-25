33a7de985792ed58dbf1af072064b805e990e174
sliptype <sliptype@gmail.com>
2019-04-24 19:51:24 -0500

    Export incorporate

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export {ScopeContext} from './context';
 export {Scope} from './scope';
 export {ReactSource} from './ReactSource';
 export {h} from './h';
+export {incorporate} from './incorporate';
 export {StreamRenderer} from './StreamRenderer';
