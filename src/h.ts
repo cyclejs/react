44e7cd7c62418b212cb069f3d152cd0f3764b418
Andre Staltz <andre@staltz.com>
2019-09-23 17:32:09 +0300

    improve h() types according to Reacts allowed types

--- src/h.ts
+++ src/h.ts
@@ -1,14 +1,24 @@
-import {createElement, ReactElement, ReactType} from 'react';
+import {
+  createElement,
+  ReactElement,
+  ReactNode,
+  ReactType,
+  Attributes,
+} from 'react';
 import {incorporate} from './incorporate';
 
 export type PropsExtensions = {
   sel?: string | symbol;
 };
 
+type PropsLike<P> = P & PropsExtensions & Attributes;
+
+type Children = string | Array<ReactNode>;
+
 function createElementSpreading<P = any>(
   type: ReactType<P>,
-  props: P | null,
-  children: string | Array<ReactElement<any>>,
+  props: PropsLike<P> | null,
+  children: Children,
 ): ReactElement<P> {
   if (typeof children === 'string') {
     return createElement(type, props, children);
@@ -19,7 +29,7 @@ function createElementSpreading<P = any>(
 
 function hyperscriptProps<P = any>(
   type: ReactType<P>,
-  props: P & PropsExtensions,
+  props: PropsLike<P>,
 ): ReactElement<P> {
   if (!props.sel) {
     return createElement(type, props);
@@ -30,15 +40,15 @@ function hyperscriptProps<P = any>(
 
 function hyperscriptChildren<P = any>(
   type: ReactType<P>,
-  children: string | Array<ReactElement<any>>,
+  children: Children,
 ): ReactElement<P> {
   return createElementSpreading(type, null, children);
 }
 
 function hyperscriptPropsChildren<P = any>(
   type: ReactType<P>,
-  props: P & PropsExtensions,
-  children: string | Array<ReactElement<any>>,
+  props: PropsLike<P>,
+  children: Children,
 ): ReactElement<P> {
   if (!props.sel) {
     return createElementSpreading(type, props, children);
@@ -49,14 +59,14 @@ function hyperscriptPropsChildren<P = any>(
 
 export function h<P = any>(
   type: ReactType<P>,
-  a?: (P & PropsExtensions) | string | Array<ReactElement<any>>,
-  b?: string | Array<React.ReactElement<any>>,
+  a?: PropsLike<P> | Children,
+  b?: Children,
 ): ReactElement<P> {
   if (a === undefined && b === undefined) {
     return createElement(type, null);
   }
   if (b === undefined && (typeof a === 'string' || Array.isArray(a))) {
-    return hyperscriptChildren(type, a as string | Array<ReactElement<any>>);
+    return hyperscriptChildren(type, a as Array<ReactNode>);
   }
   if (b === undefined && typeof a === 'object' && !Array.isArray(a)) {
     return hyperscriptProps(type, a);
